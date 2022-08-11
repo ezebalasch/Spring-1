57fc931d3830e92aa9b9d0ab51292770c7595f3e
Ezequiel Balasch <ezebalasch@gmail.com>
2022-08-11 00:20:54 -0300

    Spring 2° día

---
+++ biblioteca/src/main/java/com/egg/biblioteca/excepciones/MiException.java
@@ -0,0 +1,19 @@
+/*
+// Curso Egg FullStack
+ */
+package com.egg.biblioteca.excepciones;
+
+/**
+ *
+ * @author Ezequiel Balasch
+ */
+public class MiException extends Exception{
+
+    public MiException() {
+    }
+    
+    public MiException(String msg){
+        super(msg);
+    }
+    
+}
--- biblioteca/src/main/java/com/egg/biblioteca/servicios/AutorServicio.java
+++ biblioteca/src/main/java/com/egg/biblioteca/servicios/AutorServicio.java
@@ -4,6 +4,7 @@
 package com.egg.biblioteca.servicios;
 
 import com.egg.biblioteca.entidades.Autor;
+import com.egg.biblioteca.excepciones.MiException;
 import com.egg.biblioteca.repositorios.AutorRepositorio;
 import java.util.ArrayList;
 import java.util.List;
@@ -18,30 +19,38 @@ import org.springframework.transaction.annotation.Transactional;
  */
 @Service
 public class AutorServicio {
-    
-    @Autowired AutorRepositorio autorRepositorio;
-    
+
+    @Autowired
+    AutorRepositorio autorRepositorio;
+
     @Transactional
-    public void crearAutor(String nombre){
-         
+    public void crearAutor(String nombre) throws MiException {
+        validar(nombre);
         Autor autor = new Autor();
         autor.setNombre(nombre);
-        autorRepositorio.save(autor);   
+        autorRepositorio.save(autor);
+
     }
-    
-    public List<Autor> listarAutores(){
-        
+
+    private void validar(String nombre) throws MiException {
+        if (nombre.isEmpty() || nombre == null) {
+            throw new MiException("El nombre del autor no puede ser nulo o estar vacío");
+        }
+    }
+
+    public List<Autor> listarAutores() {
+
         List<Autor> autores = new ArrayList();
-        
+
         autores = autorRepositorio.findAll();
-        
+
         return autores;
     }
-    
-    public void modificarAutor(String nombre, String id){
-        
+
+    public void modificarAutor(String nombre, String id) {
+
         Optional<Autor> respuesta = autorRepositorio.findById(id);
-        
+
         if (respuesta.isPresent()) {
             Autor autor = respuesta.get();
             autor.setNombre(nombre);
